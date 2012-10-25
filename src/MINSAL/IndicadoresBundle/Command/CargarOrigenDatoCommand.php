dff24b0bf81eedafa3498a72b634be81eec8bc3c
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2012-10-25 11:41:49 -0600

    Se quitó la entidad SentenciaSql y su funcionalidad se pasó a OrigenDato

---
+++ src/MINSAL/IndicadoresBundle/Command/CargarOrigenDatoCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MINSAL\IndicadoresBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CargarOrigenDatoCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('origen-dato:cargar')
+            ->setDescription('Cargar datos especificados en los orígenes')            
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
