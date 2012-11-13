42f13c2dfe1c6d8c01f33ef54014e18c81063c55
rigosv <kaiba@kaiba-PC>
2012-11-12 23:22:28 -0600

    Optimización  del tiempo de carga de datos

--- src/MINSAL/IndicadoresBundle/Consumer/GuardarRegistroOrigenDatoConsumer.php
+++ src/MINSAL/IndicadoresBundle/Consumer/GuardarRegistroOrigenDatoConsumer.php
@@ -21,18 +21,19 @@ class GuardarRegistroOrigenDatoConsumer implements ConsumerInterface {
 
         // Si se retorna falso se enviará un mensaje que le indicará al producer que no se pudo procesar
         // correctamente el mensaje y será enviado nuevamente
-        $datos = '';
-        foreach ($msg['datos'] as $campo=>$valor){
-            $datos .= "'$campo','$valor',"; 
-        }
-        
-        $datos = trim($datos,',');
         
         $sql = "INSERT INTO fila_origen_dato(id, id_origen_dato, datos) 
-                    VALUES (nextval('fila_origen_dato_id_seq'::regclass), '$msg[id_origen_dato]', hstore(ARRAY[$datos]))";
+                    VALUES ";
+                
+        foreach($msg['datos'] as $fila) {
+            $llaves = "'".implode("','",array_keys($fila))."'";
+            $valores = "'".implode("','",array_values($fila))."'";
+            $sql .= "(nextval('fila_origen_dato_id_seq'::regclass), '$msg[id_origen_dato]', hstore(ARRAY[$llaves], ARRAY[$valores])), ";
+        }    
+        $sql = trim ($sql,', ');        
         
         try {
-            if ($this->em->getConnection()->exec($sql) == 1)
+            if ($this->em->getConnection()->exec($sql) >0 )
                 return true;
             else
                 return false;
@@ -41,7 +42,7 @@ class GuardarRegistroOrigenDatoConsumer implements ConsumerInterface {
         } catch (Doctrine\DBAL\DBALException $e) {
             return false;
         }
-        return new Response('');
+        return true;
     }
 
 }
