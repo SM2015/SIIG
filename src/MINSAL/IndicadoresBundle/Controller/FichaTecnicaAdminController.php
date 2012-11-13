b60a01b09d53898b6f0b95d0ddda74c3cec8b169
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2012-11-13 15:34:56 -0600

    Prueba de crear tabla de datos del indicador

---
+++ src/MINSAL/IndicadoresBundle/Controller/FichaTecnicaAdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MINSAL\IndicadoresBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+
+//use Symfony\Component\Console\Input\ArrayInput;
+
+class FichaTecnicaAdminController extends Controller {    
+    public function editAction($id = null)
+    {
+        $repo = $this->getDoctrine()->getEntityManager()->getRepository('IndicadoresBundle:FichaTecnica');        
+        $this->admin->setRepository($repo);
+        
+        return parent::editAction($id);
+
+    }
+    
+    public function createAction()
+    {
+        $repo = $this->getDoctrine()->getEntityManager()->getRepository('IndicadoresBundle:FichaTecnica');        
+        $this->admin->setRepository($repo);
+        
+        return parent::createAction();
+
+    }
+}
