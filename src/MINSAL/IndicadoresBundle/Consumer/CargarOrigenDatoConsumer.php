83fe20632ed6c7d9e4f673486756a9c08c79d41c
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2012-11-22 11:16:21 -0600

    Optimizada la carga de datos, para grandes volúmenes de datos se lee por bloques

--- src/MINSAL/IndicadoresBundle/Consumer/CargarOrigenDatoConsumer.php
+++ src/MINSAL/IndicadoresBundle/Consumer/CargarOrigenDatoConsumer.php
@@ -20,56 +20,78 @@ class CargarOrigenDatoConsumer implements ConsumerInterface {
         $msg = unserialize($msg->body);
         $em = $this->container->get('doctrine.orm.entity_manager');
 
-        $origenDato = $em->find('IndicadoresBundle:OrigenDatos', $msg['id_origen_dato']);
-        $datos = $em->getRepository('IndicadoresBundle:OrigenDatos')->getDatos($origenDato);
+        $idOrigen = $msg['id_origen_dato'];
+        $origenDato = $em->find('IndicadoresBundle:OrigenDatos', $idOrigen);
+        
+        $origenDato_aux = $origenDato;
+        $total_registros = $em->getRepository('IndicadoresBundle:OrigenDatos')->getTotalRegistros($origenDato_aux);
 
-        if ($datos != false) {
+        // Recuperar el nombre y significado de los campos del origen de datos
+        $campos_sig = array();
+        $campos = $origenDato->getCampos();
+        foreach ($campos as $campo) {
+            $campos_sig[$campo->getNombre()] = $campo->getSignificado()->getCodigo();
+        }        
+        if ($total_registros > 0) {
             //Borrar los datos existentes por el momento así será pero debería haber una forma de ir a traer solo los nuevos
             $sql = "DELETE FROM fila_origen_dato WHERE id_origen_dato='$msg[id_origen_dato]' ";
             $em->getConnection()->exec($sql);
 
-            // Recuperar el nombre y significado de los campos del origen de datos
-            foreach ($origenDato->getCampos() as $campo) {
-                $campos_sig[$campo->getNombre()] = $campo->getSignificado()->getCodigo();
-            }
-            //Esta cola la utilizaré solo para leer todos los datos y luego mandar uno por uno
-            // a otra cola que se encarará de guardarlo en la base de datos
-            // luego se puede probar a mandar por grupos       
-            $datos_a_enviar = array();
-            $i = 0;
-            $ii=0;
-            foreach ($datos as $fila) {
-                $nueva_fila = array();
-                foreach ($fila as $k => $v) {
-                    $nueva_fila[$campos_sig[$k]] = $v;
+            //Leeré los datos en grupos de 100,000
+            $tamanio = 100000;            
+            if ($total_registros > $tamanio) {
+                $partes = ceil($total_registros / $tamanio);
+                $sql = $origenDato_aux->getSentenciaSql();
+                for ($i = 0; $i < $partes; $i++) {
+                    $sql_aux = $sql . ' LIMIT ' . $tamanio . ' OFFSET ' . $i * $tamanio;
+                    $origenDato_aux->setSentenciaSql($sql_aux);
+                    $datos = $em->getRepository('IndicadoresBundle:OrigenDatos')->getDatos($origenDato_aux);
+                    $this->enviarDatos($idOrigen, $datos, $campos_sig);
                 }
-                $datos_a_enviar[] = $nueva_fila;
-                //Enviaré en grupos de 200
-                if ($i == 200) {                                        
-                    $msg_guardar = array('id_origen_dato' => $msg['id_origen_dato'],
-                        'datos' => $datos_a_enviar,
-                        'num_msj'=>$ii++);
-                    $this->container->get('old_sound_rabbit_mq.guardar_registro_producer')
-                            ->publish(serialize($msg_guardar));
-                    unset($datos_a_enviar);
-                    $datos_a_enviar = array();
-                    $i=0;                    
-                } 
-                $i++;
+            } else {
+                $datos = $em->getRepository('IndicadoresBundle:OrigenDatos')->getDatos($origenDato);                
+                $this->enviarDatos($idOrigen, $datos, $campos_sig);
             }
-            //Verificar si quedaron pendientes de enviar
-            if (count($datos_a_enviar) > 0) {
-                $msg_guardar = array('id_origen_dato' => $msg['id_origen_dato'],
-                    'datos' => $datos_a_enviar,
-                    'num_msj'=>$ii++);
-                $this->container->get('old_sound_rabbit_mq.guardar_registro_producer')
-                        ->publish(serialize($msg_guardar));
-            }
-
             return true;
         }
         else
             return false;
+    }
+
+    public function enviarDatos($idOrigen, $datos, $campos_sig) {
+        //Esta cola la utilizaré solo para leer todos los datos y luego mandar uno por uno
+        // a otra cola que se encarará de guardarlo en la base de datos
+        // luego se puede probar a mandar por grupos       
+        $datos_a_enviar = array();
+        $i = 0;
+        $ii = 0;
+        foreach ($datos as $fila) {
+            $nueva_fila = array();
+            foreach ($fila as $k => $v) {
+                $nueva_fila[$campos_sig[$k]] = $v;
+            }
+            $datos_a_enviar[] = $nueva_fila;
+            //Enviaré en grupos de 400
+            if ($i == 200) {
+                $msg_guardar = array('id_origen_dato' => $idOrigen,
+                    'datos' => $datos_a_enviar,
+                    'num_msj' => $ii++);
+                $this->container->get('old_sound_rabbit_mq.guardar_registro_producer')
+                        ->publish(serialize($msg_guardar));
+                unset($datos_a_enviar);
+                $datos_a_enviar = array();
+                $i = 0;
+            }
+            $i++;
+        }
+        //Verificar si quedaron pendientes de enviar
+        if (count($datos_a_enviar) > 0) {
+            $msg_guardar = array('id_origen_dato' => $idOrigen,
+                'datos' => $datos_a_enviar,
+                'num_msj' => $ii++);
+            $this->container->get('old_sound_rabbit_mq.guardar_registro_producer')
+                    ->publish(serialize($msg_guardar));
+        }
     }
 
 }
