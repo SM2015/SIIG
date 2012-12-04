37c9d469948e49e79eb3bab6eede3f97f5158ee3
rigosv <kaiba@kaiba-PC>
2012-12-03 22:14:05 -0600

    agregado script para iniciar las colas de carga de datos

---
+++ src/MINSAL/IndicadoresBundle/Util/iniciar_colas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PHP=`which php`
+CONSOLE="$PHP ../app/console --env=prod"
+
+cd "$( dirname "${BASH_SOURCE[0]}" )";
+
+PATH="../../../.."
+
+#CONSUMERS=`$CONSOLE MINSAL:queue:get_all_consumers`
+
+#for CONSUMER in $CONSUMERS
+#    do
+/usr/bin/nohup $PHP $PATH/app/console rabbitmq:consumer cargar_origen_datos > $PATH"/app/logs/rabbitmq_cargar_origen_datos.log" &2>1 &
+/usr/bin/nohup $PHP $PATH/app/console rabbitmq:consumer guardar_registro > $PATH"/app/logs/rabbitmq_guardar_registro.log" &2>1 &
+#done
+
