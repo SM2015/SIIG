6b6e1df8f6c5834cc6f52f54a9c07af24746f4c1
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2013-02-26 13:25:57 -0600

    Reestructuración del código de la plantilla del tablero para prepararla para la matriz de indicadores

--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_columnas.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_columnas.js
@@ -1,127 +1,149 @@
-graficoColumnas = function (ubicacion, datos, colorChosen, categoryChoosen) {
+graficoColumnas = function(ubicacion, datos, colorChosen, categoryChoosen) {
 
-    this.tipo = 'columnas';    
+    this.tipo = 'columnas';
     var margin = {top: 0, right: 5, bottom: 25, left: 70},
-        width = 450 - margin.left - margin.right,
+    width = 450 - margin.left - margin.right,
             height = 300 - margin.top - margin.bottom,
             barPadding = 1
             ;
 
-    var currentDatasetChart = datos;    
+    var currentDatasetChart = datos;
+    var zona = ubicacion;
     var xScale = d3.scale.ordinal()
-            .domain(currentDatasetChart.map(function(d) { return d.category; }))            
+            .domain(currentDatasetChart.map(function(d) {
+        return d.category;
+    }))
             .rangeRoundBands([0, width], .1)
             ;
 
     var max_y;
-    
+
     //El nivel máximo de la escala puede ser el mayor valor de la serie
     // o el mayor valor del rango, el usuario elige
     // se utiliza datasetPrincipal_bk por si se han aplicado filtros
     // Así no usará el máximo valor del filtro
-    max_y = d3.max(datasetPrincipal_bk, function(d) { return parseFloat(d.measure); });
-    if ($('#max_y') != null && $('#max_y').val()=='rango_alertas')
-        max_y = $('#titulo_indicador').attr('data-max_rango');
-    
+    max_y = d3.max(datasetPrincipal_bk, function(d) {
+        return parseFloat(d.measure);
+    });
+    if ($('#' + ubicacion + ' .max_y') != null && $('#' + ubicacion + ' .max_y').val() == 'rango_alertas')
+        max_y = $('#' + zona + ' .titulo_indicador').attr('data-max_rango');
+
     var yScale = d3.scale.linear()
             .domain([0, max_y])
             .range([height, 0])
             ;
-       
+
     var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
     var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-
-    $('#' + ubicacion).html('');        
-    var svg = d3.select("#" + ubicacion)
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)    
-            .attr("id","ChartPlot")
-            ;
-
-    var plot = svg
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top +5 + ")")
-            ;
-    var long = $('#titulo_indicador').attr('data-unidad-medida').length;
     
-    svg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(" + margin.left + ",5)")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", -50)        
-        .attr("x", -((height/2) + (long / 2)*6.5))
-        .text($('#titulo_indicador').attr('data-unidad-medida'));
+    // Dibuja el gráfico
+    this.dibujar = function() {
+        $('#' + ubicacion + ' .grafico').html('');
+            var svg = d3.select("#" + ubicacion + ' .grafico')
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .attr("id", "ChartPlot")
+                ;
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(" + margin.left + "," + (margin.top + height+5) + ")")
-      .call(xAxis);
-    
-    plot.selectAll("rect")
-        .data(currentDatasetChart)        
-        .enter()
-        .append("rect")
-        .attr("stroke", 'black')
-        .attr("stroke-width", '2px')
-        .attr("x", function(d, i) {
+        var plot = svg
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + 5 + ")")
+                ;
+        var long = $('#' + ubicacion + ' .titulo_indicador').attr('data-unidad-medida').length;
+
+        svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + margin.left + ",5)")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -50)
+                .attr("x", -((height / 2) + (long / 2) * 6.5))
+                .text($('#' + ubicacion + ' .titulo_indicador').attr('data-unidad-medida'));
+
+        svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + margin.left + "," + (margin.top + height + 5) + ")")
+                .call(xAxis);
+
+        plot.selectAll("rect")
+                .data(currentDatasetChart)
+                .enter()
+                .append("rect")
+                .attr("stroke", 'black')
+                .attr("stroke-width", '2px')
+                .attr("x", function(d, i) {
             return xScale(d.category);
         })
-        .transition().duration(1000).delay(20)
-        .attr("width", xScale.rangeBand())
-        .attr("y", function(d) {
+                .transition().duration(1000).delay(20)
+                .attr("width", xScale.rangeBand())
+                .attr("y", function(d) {
             return yScale(parseFloat(d.measure));
-        })        
-        .attr("height", function(d) {
+        })
+                .attr("height", function(d) {
             return height - yScale(parseFloat(d.measure));
-        });        
-    plot.selectAll("rect").append("title")
-        .text(function(d) {
+        });
+        plot.selectAll("rect").append("title")
+                .text(function(d) {
             return d.category + ": " + d.measure;
         });
-        
-    if (ubicacion == 'graficoPrimario')
+
         plot.selectAll("rect").on("click", function(d, i) {
-            descenderNivelDimension(d.category);
-        });    
-    if (colorChosen == null)
-        plot.selectAll("rect").attr("fill", function(d, i) {            
-            //evaluar que color le corresponde
-            return colores_alertas(d.measure, i) 
-        }); 
-    else
-        plot.selectAll("rect").attr("fill", colorChosen);    
-
-this.ordenar = function(modo_orden, ordenar_por) {
-    //clearTimeout(sortTimeout);
-
-    // Copy-on-write since tweens are evaluated after a delay.
-    if (ordenar_por=='dimension')
-        var x0 = xScale.domain(currentDatasetChart.sort(
-            (modo_orden=='asc') ? 
-            function(a, b) { return d3.ascending((isNaN(a.category))?a.category:parseFloat(a.category), (isNaN(b.category))?b.category:parseFloat(b.category)); }:
-            function(a, b) { return d3.descending((isNaN(a.category))?a.category:parseFloat(a.category), (isNaN(b.category))?b.category:parseFloat(b.category)); }
-        ).map(function(d) { return d.category; }))
-        .copy();
-    else
-        var x0 = xScale.domain(currentDatasetChart.sort(
-            (modo_orden=='asc') ? function(a, b) { return d3.ascending(parseFloat(a.measure), parseFloat(b.measure)); }:
-            function(a, b) { return d3.descending(parseFloat(a.measure), parseFloat(b.measure)); }
+            descenderNivelDimension(ubicacion, d.category);
+        });
+        if (colorChosen == null)
+            plot.selectAll("rect").attr("fill", function(d, i) {
+                //evaluar que color le corresponde
+                return colores_alertas(ubicacion, d.measure, i)
+            });
+        else
+            plot.selectAll("rect").attr("fill", colorChosen);
+    };
+    this.ordenar = function(modo_orden, ordenar_por) {
+        //clearTimeout(sortTimeout);
+        var svg = d3.select("#" + zona + ' .grafico');
+        // Copy-on-write since tweens are evaluated after a delay.
+        if (ordenar_por === 'dimension')
+            var x0 = xScale.domain(currentDatasetChart.sort(
+                    (modo_orden === 'asc') ?
+                    function(a, b) {
+                        return d3.ascending((isNaN(a.category)) ? a.category : parseFloat(a.category), (isNaN(b.category)) ? b.category : parseFloat(b.category));
+                    } :
+                    function(a, b) {
+                        return d3.descending((isNaN(a.category)) ? a.category : parseFloat(a.category), (isNaN(b.category)) ? b.category : parseFloat(b.category));
+                    }
+            ).map(function(d) {
+                return d.category;
+            }))
+                    .copy();
+        else
+            var x0 = xScale.domain(currentDatasetChart.sort(
+                    (modo_orden == 'asc') ? function(a, b) {
+                return d3.ascending(parseFloat(a.measure), parseFloat(b.measure));
+            } :
+                    function(a, b) {
+                        return d3.descending(parseFloat(a.measure), parseFloat(b.measure));
+                    }
             )
-        .map(function(d) { return d.category; }))
-        .copy();
-    var transition = svg.transition().duration(750),
-        delay = function(d, i) { return i * 70; };
+                    .map(function(d) {
+                return d.category;
+            }))
+                    .copy();
+        var transition = svg.transition().duration(750),
+                delay = function(d, i) {
+            return i * 70;
+        };
 
-    transition.selectAll("rect")
-        .delay(delay)
-        .attr("x", function(d) { return x0(d.category); });
+        transition.selectAll("#"+ubicacion+" rect")
+                .delay(delay)
+                .attr("x", function(d) {
+            return x0(d.category);
+        });
 
-    transition.select(".x.axis")
-        .call(xAxis)
-      .selectAll("g")
-        .delay(delay);
-  }    
+        transition.select('#'+ubicacion+' .x.axis')
+                .call(xAxis)
+                .selectAll("g")
+                .delay(delay);
+    }
 }
--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_lineas.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_lineas.js
@@ -6,113 +6,135 @@ graficoLineas = function(ubicacion, datos, colorChosen, categoryChoosen) {
             height = 300 - margin.top - margin.bottom
             ;
     var currentDatasetChart = datos;
+    var zona = ubicacion;
 
     var xScale = d3.scale.ordinal()
-        .domain(currentDatasetChart.map(function(d) { return d.category; }))
-        .rangeRoundBands([0, width], .9);
+            .domain(currentDatasetChart.map(function(d) {
+        return d.category;
+    }))
+            .rangeRoundBands([0, width], .9);
     ;
     var max_y;
-    max_y = d3.max(datasetPrincipal_bk, function(d) { return parseFloat(d.measure); });
-    if ($('#max_y') != null && $('#max_y').val()=='rango_alertas')
-        max_y = $('#titulo_indicador').attr('data-max_rango');
-    
+    max_y = d3.max(datasetPrincipal_bk, function(d) {
+        return parseFloat(d.measure);
+    });
+    if ($('#' + ubicacion + ' .max_y') != null && $('#' + ubicacion + ' .max_y').val() == 'rango_alertas')
+        max_y = $('#' + ubicacion + ' .titulo_indicador').attr('data-max_rango');
+
     var yScale = d3.scale.linear()
             .domain([0, max_y])
             .range([height, 0]);
-    
+
     var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
     var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
     var line = d3.svg.line()
             .x(function(d, i) {
-                return xScale(d.category);
-            })
+        return xScale(d.category);
+    })
             .y(function(d) {
-                return yScale(parseFloat(d.measure));
-            });
+        return yScale(parseFloat(d.measure));
+    });
 
-    $('#' + ubicacion).html('');
-    var svg = d3.select("#" + ubicacion).append("svg")
-            .datum(currentDatasetChart)
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .attr("id","ChartPlot")
-    // create group and move it so that margins are respected (space for axis and title)
+    this.dibujar = function() {
+        $('#' + ubicacion + ' .grafico').html('');
+        var svg = d3.select("#" + ubicacion + ' .grafico ')
+                .append("svg")
+                .datum(currentDatasetChart)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .attr("id", "ChartPlot")
+        // create group and move it so that margins are respected (space for axis and title)
 
-    var plot = svg
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            ;
+        var plot = svg
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                ;
 
-    var long = $('#titulo_indicador').attr('data-unidad-medida').length;
-    svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .call(yAxis).append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", -50)        
-            .attr("x", -((height/2) + (long / 2)*6.5))          
-            .text($('#titulo_indicador').attr('data-unidad-medida'));
+        var long = $('#' + ubicacion + ' .titulo_indicador').attr('data-unidad-medida').length;
+        svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .call(yAxis).append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -50)
+                .attr("x", -((height / 2) + (long / 2) * 6.5))
+                .text($('#' + ubicacion + ' .titulo_indicador').attr('data-unidad-medida'));
 
-            
-    svg.append("g")
-      .transition().duration(1000).delay(20)
-        .attr("class", "x axis")      
-      .attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
-      .call(xAxis);
-      
-    plot.append("path")
-            .attr("class", "line")
-            .attr("d", line)
-            .attr("stroke", 'blue')
-            ;
 
-    plot.selectAll(".dot")
-            .data(currentDatasetChart)
-            .enter().append("circle")
-            .attr("class", "dot")
-            .transition().duration(1000).delay(20)
-            .attr("fill", function(d, i) {
-        return colores_alertas(d.measure, i)
-    })
-            .attr("cx", line.x())
-            .attr("cy", line.y())
-            .attr("r", 7.5)
-            .attr("stroke", colorChosen);
-    plot.selectAll(".dot")
-            .append("title")
-            .text(function(d) {
-        return d.category + ": " + d.measure;
-    })
-            ;
+        svg.append("g")
+                .transition().duration(1000).delay(20)
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
+                .call(xAxis);
 
-    plot.selectAll(".dot").on("click", function(d, i) {
-        descenderNivelDimension(d.category);
-    })
+        plot.append("path")
+                .attr("class", "line")
+                .attr("d", line)
+                .attr("stroke", 'blue')
+                ;
 
-    
+        plot.selectAll(".dot")
+                .data(currentDatasetChart)
+                .enter().append("circle")
+                .attr("class", "dot")
+                .transition().duration(1000).delay(20)
+                .attr("fill", function(d, i) {
+            return colores_alertas(ubicacion, d.measure, i)
+        })
+                .attr("cx", line.x())
+                .attr("cy", line.y())
+                .attr("r", 7.5)
+                .attr("stroke", colorChosen);
+        plot.selectAll(".dot")
+                .append("title")
+                .text(function(d) {
+            return d.category + ": " + d.measure;
+        })
+                ;
+
+        plot.selectAll(".dot").on("click", function(d, i) {
+            descenderNivelDimension(ubicacion, d.category);
+        });
+    };
     this.ordenar = function(modo_orden, ordenar_por) {
-        
+        var svg = d3.select("#" + zona + ' .grafico ');
         // Copy-on-write since tweens are evaluated after a delay.
         if (ordenar_por == 'dimension')
             var x0 = xScale.domain(currentDatasetChart.sort(
-                    (modo_orden == 'asc') ? 
-                        function(a, b) { return d3.ascending((isNaN(a.category))?a.category:parseFloat(a.category), (isNaN(b.category))?b.category:parseFloat(b.category)); }:
-                        function(a, b) { return d3.descending((isNaN(a.category))?a.category:parseFloat(a.category), (isNaN(b.category))?b.category:parseFloat(b.category)); }
-            ).map(function(d) { return d.category; })).copy();
+                    (modo_orden == 'asc') ?
+                    function(a, b) {
+                        return d3.ascending((isNaN(a.category)) ? a.category : parseFloat(a.category), (isNaN(b.category)) ? b.category : parseFloat(b.category));
+                    } :
+                    function(a, b) {
+                        return d3.descending((isNaN(a.category)) ? a.category : parseFloat(a.category), (isNaN(b.category)) ? b.category : parseFloat(b.category));
+                    }
+            ).map(function(d) {
+                return d.category;
+            })).copy();
         else
             var x0 = xScale.domain(currentDatasetChart.sort(
-                    (modo_orden == 'asc') ? 
-                        function(a, b) {return d3.ascending(a.measure, b.measure);} :
-                        function(a, b) {return d3.descending(a.measure, b.measure);}
-            ).map(function(d) { return d.category; })).copy();
-    
-        var transition = svg.transition().duration(750),
-            delay = function(d, i) {return i * 90;};
+                    (modo_orden === 'asc') ?
+                    function(a, b) {
+                        return d3.ascending(a.measure, b.measure);
+                    } :
+                    function(a, b) {
+                        return d3.descending(a.measure, b.measure);
+                    }
+            ).map(function(d) {
+                return d.category;
+            })).copy();
 
-        transition.selectAll(".line").delay(delay).attr("d", line).attr("stroke", 'blue');        
-        transition.selectAll(".dot").delay(delay).attr("cx", function(d) { return x0(d.category); });
-        transition.select(".x.axis").call(xAxis).selectAll("g").delay(delay);  
-    
+        var transition = svg.transition().duration(750),
+                delay = function(d, i) {
+            return i * 90;
+        };
+
+        transition.selectAll(".line").delay(delay).attr("d", line).attr("stroke", 'blue');
+        transition.selectAll(".dot").delay(delay).attr("cx", function(d) {
+            return x0(d.category);
+        });
+        transition.select(".x.axis").call(xAxis).selectAll("g").delay(delay);
+
     }
 }
