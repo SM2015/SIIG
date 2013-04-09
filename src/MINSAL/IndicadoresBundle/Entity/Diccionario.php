b7f49353ad121b35a46d1910fe19a37567dde701
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2013-04-08 22:51:05 -0600

    Refactorización de la base de datos para soportar la característica de diccionario de transformación

---
+++ src/MINSAL/IndicadoresBundle/Entity/Diccionario.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace MINSAL\IndicadoresBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MINSAL\IndicadoresBundle\Entity\Diccionario
+ *
+ * @ORM\Table(name="diccionario")
+ * @ORM\Entity
+ */
+class Diccionario
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $codigo
+     *
+     * @ORM\Column(name="codigo", type="string", length=20, nullable=false)
+     */
+    private $codigo;
+    
+    /**
+     * @var string $descripcion
+     *
+     * @ORM\Column(name="descripcion", type="string", length=200, nullable=false)
+     */
+    private $descripcion;
+    
+    /**
+    * @ORM\OneToMany(targetEntity="ReglaTransformacion", mappedBy="diccionario")
+    */
+    private $reglas;
+
+        
+    public function __toString() {
+        return $this->descripcion.'';
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->reglas = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     * @return Diccionario
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Add reglas
+     *
+     * @param \MINSAL\IndicadoresBundle\Entity\ReglaTransformacion $reglas
+     * @return Diccionario
+     */
+    public function addRegla(\MINSAL\IndicadoresBundle\Entity\ReglaTransformacion $reglas)
+    {
+        $this->reglas[] = $reglas;
+    
+        return $this;
+    }
+
+    /**
+     * Remove reglas
+     *
+     * @param \MINSAL\IndicadoresBundle\Entity\ReglaTransformacion $reglas
+     */
+    public function removeRegla(\MINSAL\IndicadoresBundle\Entity\ReglaTransformacion $reglas)
+    {
+        $this->reglas->removeElement($reglas);
+    }
+
+    /**
+     * Get reglas
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getReglas()
+    {
+        return $this->reglas;
+    }
+
+    /**
+     * Set codigo
+     *
+     * @param string $codigo
+     * @return Diccionario
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+    
+        return $this;
+    }
+
+    /**
+     * Get codigo
+     *
+     * @return string 
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+}
---
+++ src/MINSAL/IndicadoresBundle/Entity/ReglaTransformacion.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace MINSAL\IndicadoresBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MINSAL\IndicadoresBundle\Entity\ReglaTransformacion
+ *
+ * @ORM\Table(name="regla_transformacion")
+ * @ORM\Entity
+ */
+class ReglaTransformacion
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $regla
+     *
+     * @ORM\Column(name="regla", type="string", length=15, nullable=false)
+     */
+    private $regla;
+    
+    /**
+     * @var string $limiteInferior
+     *
+     * @ORM\Column(name="limite_inferior", type="string", length=100, nullable=false)
+     */
+    private $limiteInferior;
+    
+    /**
+     * @var string $limiteSuperior
+     *
+     * @ORM\Column(name="limite_superior", type="string", length=100, nullable=true)
+     */
+    private $limiteSuperior;
+    
+    
+    /**
+     * @var string $transformacion
+     *
+     * @ORM\Column(name="transformacion", type="string", length=100, nullable=false)
+     */
+    private $transformacion;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Diccionario", inversedBy="reglas")
+     * @ORM\JoinColumn(name="diccionario", referencedColumnName="id", onDelete="CASCADE")
+     **/
+    private $diccionario;
+    
+    
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set regla
+     *
+     * @param string $regla
+     * @return ReglaTransformacion
+     */
+    public function setRegla($regla)
+    {
+        $this->regla = $regla;
+    
+        return $this;
+    }
+
+    /**
+     * Get regla
+     *
+     * @return string 
+     */
+    public function getRegla()
+    {
+        return $this->regla;
+    }
+
+    /**
+     * Set limiteInferior
+     *
+     * @param string $limiteInferior
+     * @return ReglaTransformacion
+     */
+    public function setLimiteInferior($limiteInferior)
+    {
+        $this->limiteInferior = $limiteInferior;
+    
+        return $this;
+    }
+
+    /**
+     * Get limiteInferior
+     *
+     * @return string 
+     */
+    public function getLimiteInferior()
+    {
+        return $this->limiteInferior;
+    }
+
+    /**
+     * Set limiteSuperior
+     *
+     * @param string $limiteSuperior
+     * @return ReglaTransformacion
+     */
+    public function setLimiteSuperior($limiteSuperior)
+    {
+        $this->limiteSuperior = $limiteSuperior;
+    
+        return $this;
+    }
+
+    /**
+     * Get limiteSuperior
+     *
+     * @return string 
+     */
+    public function getLimiteSuperior()
+    {
+        return $this->limiteSuperior;
+    }
+
+    /**
+     * Set transformacion
+     *
+     * @param string $transformacion
+     * @return ReglaTransformacion
+     */
+    public function setTransformacion($transformacion)
+    {
+        $this->transformacion = $transformacion;
+    
+        return $this;
+    }
+
+    /**
+     * Get transformacion
+     *
+     * @return string 
+     */
+    public function getTransformacion()
+    {
+        return $this->transformacion;
+    }
+
+    /**
+     * Set diccionario
+     *
+     * @param \MINSAL\IndicadoresBundle\Entity\Diccionario $diccionario
+     * @return ReglaTransformacion
+     */
+    public function setDiccionario(\MINSAL\IndicadoresBundle\Entity\Diccionario $diccionario = null)
+    {
+        $this->diccionario = $diccionario;
+    
+        return $this;
+    }
+
+    /**
+     * Get diccionario
+     *
+     * @return \MINSAL\IndicadoresBundle\Entity\Diccionario 
+     */
+    public function getDiccionario()
+    {
+        return $this->diccionario;
+    }
+}
