e550ecb93755432c81c51c705c7120606310d37b
rigo <rigo@KaibaCorp>
2013-07-02 19:50:44 -0600

    Eliminación de archivo ya no utilizado

--- src/MINSAL/IndicadoresBundle/Resources/public/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    //$( "#accordion" ).accordion();
-    $("#accordion").liteAccordion({"theme":"stitch",
-        "easing":"linear", 
-        "headerWidth":60,
-        "enumerateSlides":true
-    });
-})
