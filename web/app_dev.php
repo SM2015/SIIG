19f3f2d06996139d7c08ae4c3b6a5e291a643c52
Eric Ramirez <rajotico@gmail.com>
2013-09-22 20:06:09 -0600

    Update app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
-
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
