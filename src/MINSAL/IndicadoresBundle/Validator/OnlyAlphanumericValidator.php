67c958295ca20611b3e3176e385769ef8ccdb7f7
rvidaurric <rvidaurric@gmail.com>
2013-11-12 09:22:45 -0600

    se agrega guion bajo al validador OnlyAlphanumeric

--- src/MINSAL/IndicadoresBundle/Validator/OnlyAlphanumericValidator.php
+++ src/MINSAL/IndicadoresBundle/Validator/OnlyAlphanumericValidator.php
@@ -8,7 +8,7 @@ class OnlyAlphanumericValidator extends ConstraintValidator
 {
 	public function validate($value, Constraint $constraint)
 	{
-		if (!preg_match('/^[a-zA-Za0-9 ]+$/', utf8_decode($value), $matches)) {
+		if (!preg_match('/^[a-zA-Za0-9 _]+$/', utf8_decode($value), $matches)) {
 			$this->context->addViolation($constraint->message, array('%string%' => $value));
 		}
 	}
