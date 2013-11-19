ea29855c4bd5491ebb394647ea2d5c32ce56c783
rigosv <rigoberto.reyes@gmail.com>
2013-11-18 21:06:57 -0600

    Compartir Salas y agregar comentarios a sala, reestructuración del menú del tablero

--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/tablero.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/tablero.js
@@ -5,40 +5,55 @@ $(document).ready(function() {
     //Si se despliega algún menú dentro del gráfico se modifica un atributo
     //ccs para que se muestre correctamente se regresa a su modo normal cuando el menú se cierra
     //para esto fue necesario reescribir unos métodos de jQuery
-    (function(){
+    (function() {
         var methods = ["addClass", "toggleClass", "removeClass"]; //métodos a sobreescribir
-        $.map(methods, function(method){
-            var originalMethod = $.fn[ method ];            
-            $.fn[ method ] = function(){                
-                var result = originalMethod.apply( this, arguments ); // Execute the original method.                
+        $.map(methods, function(method) {
+            var originalMethod = $.fn[ method ];
+            $.fn[ method ] = function() {
+                var result = originalMethod.apply(this, arguments); // Execute the original method.                
                 myfunction(this); // call your function                
                 return result; // return the original result
             };
         });
     })();
-    
-    function myfunction(obj){
-        if($(obj).hasClass('sobre_div'))
+
+    function myfunction(obj) {
+        if ($(obj).hasClass('sobre_div'))
             if ($(obj).hasClass('open'))
-                $('.zona_actual').css('overflow-y','visible');            
+                $('.zona_actual').css('overflow-y', 'visible');
             else
-                $('.area_grafico').filter(function(){ return $(this).css('overflow-y') === 'visible';}).css('overflow-y','auto');        
-    }    
+                $('.area_grafico').filter(function() {
+                    return $(this).css('overflow-y') === 'visible';
+                }).css('overflow-y', 'auto');
+    }
     // *****************
+    $('#myTab a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+    $('#collapseOne').toggle();
+    $('#aCollapseOne').click(function() {
+        $('#collapseOne').toggle();
+    });
+
     
-    jQuery(document).ajaxStart(function() {
-        $('#div_carga').show();
-    }).ajaxStop(function() {
-        $('#div_carga').hide();
+    function ajax_states(){
+        $(document).bind("ajaxStart.mine", function() {
+            $('#div_carga').show();
+        });
+        $(document).bind("ajaxStop.mine", function() {
+            $('#div_carga').hide();
+        });
+    }
+    ajax_states();
+    $("#sala").sortable({
+        handle: '.titulo',
     });
-            
-    $( "#sala" ).sortable({
-        handle: '.titulo',                
-    });
-    $( "#sala" ).disableSelection();
-        
+    $("#sala").disableSelection();
+
     $('A.indicador').click(function() {
         dibujarIndicador($(this).attr('data-id'));
+        moverAGraficoActual();
     });
 
     function dibujarIndicador(id_indicador) {
@@ -46,11 +61,12 @@ $(document).ready(function() {
     }
 
     $('#agregar_fila').click(function() {
-        sala_agregar_fila();        
+        sala_agregar_fila();
     });
 
     $('#quitar_indicador').click(function() {
         limpiarZona2($('DIV.zona_actual').attr('id'));
+        moverAGraficoActua();
     });
 
     function ver_ficha_tecnica(id_indicador) {
@@ -68,19 +84,19 @@ $(document).ready(function() {
 
     function sala_agregar_fila() {
         var cant = $('DIV.area_grafico').length;
-        var html =  '<div class="area_grafico" id="grafico_' + parseInt(cant+1) + '" >' +
-                        "<DIV class= 'titulo'><span class='titulo_indicador '></span>"+
-                            "<span>("+trans.por+" <span class='dimension' ></span>)</span>"+
-                        '</DIV>'+
-                        '<h6 class="filtros_dimensiones"></h6>' +
-                        '<div class="controles btn-toolbar"></div>' +
-                        '<div class="row-fluid info" ></div>' +
-                        '<div class="row-fluid row_grafico" >' +
-                            '<div class="grafico" ></div>' +
-                        '</div>' +                        
-                    '</DIV>';         
+        var html = '<div class="area_grafico" id="grafico_' + parseInt(cant + 1) + '" >' +
+                "<DIV class= 'titulo'><span class='titulo_indicador '></span>" +
+                "<span>(" + trans.por + " <span class='dimension' ></span>)</span>" +
+                '</DIV>' +
+                '<h6 class="filtros_dimensiones"></h6>' +
+                '<div class="controles btn-toolbar"></div>' +
+                '<div class="row-fluid info" ></div>' +
+                '<div class="row-fluid row_grafico" >' +
+                '<div class="grafico" ></div>' +
+                '</div>' +
+                '</DIV>';
 
-        $('#sala').append(html);        
+        $('#sala').append(html);
         $('DIV.area_grafico').click(function() {
             zona_elegir(this);
         });
@@ -101,14 +117,14 @@ $(document).ready(function() {
             if ($(this).find('.titulo_indicador').html() !== '') {
                 var datos = new Object();
                 var elementos = [];
-                $('#'+ $(this).attr('id') +' .capa_dimension_valores input:checked').each(function() {
+                $('#' + $(this).attr('id') + ' .capa_dimension_valores input:checked').each(function() {
                     elementos.push($(this).val());
-                }); 
-                
+                });
+
                 datos.id_indicador = $(this).children('.titulo_indicador').attr('data-id');
                 datos.filtros = $(this).children('.filtros_dimensiones').attr('data');
-                datos.filtro_desde = $('#'+$(this).attr('id')+' .filtro_desde').val();
-                datos.filtro_hasta = $('#'+$(this).attr('id')+' .filtro_hasta').val();
+                datos.filtro_desde = $('#' + $(this).attr('id') + ' .filtro_desde').val();
+                datos.filtro_hasta = $('#' + $(this).attr('id') + ' .filtro_hasta').val();
                 datos.filtro_elementos = elementos.toString();
                 datos.dimension = $('#' + $(this).attr('id') + ' .dimensiones').val();
                 datos.tipo_grafico = $('#' + $(this).attr('id') + ' .tipo_grafico_principal').val();
@@ -128,7 +144,7 @@ $(document).ready(function() {
         function(resp) {
             if (resp.estado === 'ok') {
                 $('#nombre_sala').attr('id-sala', resp.id_sala);
-                $('#nombre_sala2').html('<h4>Nombre de sala: ' + $('#nombre_sala').val() + '</h4>');
+                $('.marco-sala').attr('data-content', trans._nombre_sala_ + ': ' + $('#nombre_sala').val());
                 $('#myModal').modal('toggle');
             }
             else {
@@ -139,9 +155,10 @@ $(document).ready(function() {
     });
 
     $('.salas-id').click(function() {
+        $('.marco-sala').attr('id-sala', $(this).attr('sala-id'));
+        $('.marco-sala').attr('data-content', trans._nombre_sala_ + ': ' + $(this).attr('sala-nombre'));
         $('#nombre_sala').attr('id-sala', $(this).attr('sala-id'));
-        $('#nombre_sala').val($(this).attr('sala-nombre'));
-        $('#nombre_sala2').html('<h4>Nombre de sala: ' + $(this).attr('sala-nombre') + '</h4>');
+        $('#nombre_sala').html($(this).attr('sala-nombre'));
 
         var graficos = JSON.parse($(this).attr('data'));
         var max_id = 0;
@@ -151,18 +168,93 @@ $(document).ready(function() {
         }
 
         $('#sala').html('');
-        
+
         var filas = Math.ceil(max_id / 3);
         for (i = 1; i <= max_id; i++) {
             sala_agregar_fila();
         }
-        
+
         for (i = 0; i < graficos.length; i++) {
             $('DIV.zona_actual').removeClass('zona_actual');
             $('#grafico_' + graficos[i].posicion).addClass('zona_actual');
 
             recuperarDimensiones(graficos[i].idIndicador, graficos[i]);
         }
-    });   
+        $('#myTab a:first').tab('show');
+        $('#listado-salas li').removeClass('active');
+        $('li[sala-id="' + $('.marco-sala').attr('id-sala') + '"]').addClass('active');
+        
+        cargarMensajes();
+        cargarUsuarios();
+    });
+
+    function moverAGraficoActual() {
+        $('#myTab a:first').tab('show');
+        $('html, body').animate({
+            scrollTop: $(".zona_actual").offset().top
+        }, 2000);
+    }
+
+    function setScroll() {
+        $('#chat-mensajes').animate({
+            scrollTop: $("div.mensaje-sala:last")
+        });
+    }
+
+    // Indicamos las acciones a ejecutar al enviar un mensaje
+    $('#chat-form').submit(function() {
+        if ($('.marco-sala').attr('id-sala')) {
+            $.post(Routing.generate('sala_set_comentario', {id: $('.marco-sala').attr('id-sala')}), $('#chat-form').serialize(), function(data) {
+                $('#chat-mensajes').append(data);   // Añadir el nuevo mensaje al final
+                $('#chat-form-mensaje').val('');        // Limpiar el cuadro de texto
+                setScroll();
+            });
+        }
+        else {
+            alert(trans._no_sala_);
+        }
+        return false;
+    });
+
+    $("#chat-mensajes").unbind("ajaxStart ajaxStop");
+
+    function cargarMensajes() {
+        if ($('.marco-sala').attr('id-sala')) {
+            $('#chat-mensajes').load(
+                    Routing.generate('sala_get_comentarios', {idSala: $('.marco-sala').attr('id-sala')}), {vez: 1},
+            function(response, status, xhr) {                
+                setScroll();                
+            });
+        }
+    }
     
+    function cargarUsuarios(){
+        if ($('.marco-sala').attr('id-sala')) {
+            $('#usuarios_sala').load(
+                    Routing.generate('sala_get_usuarios', {idSala: $('.marco-sala').attr('id-sala')}),
+            function(response, status, xhr) {
+                $('.usuariosSala').change(function(){
+                    var accion = ($(this).is(':checked')) ? 'agregar' : 'borrar';
+                    $.get(Routing.generate('sala_set_usuario', 
+                        {id: $('.marco-sala').attr('id-sala'), id_usuario: $(this).val(), accion: accion}
+                    ));
+                });
+            });
+        }
+    }
+
+
+    // Le indicamos cargar los mensajes cada minuto
+    setInterval(function() {
+        if ($('.marco-sala').attr('id-sala')) {
+            $(document).unbind(".mine");
+            $.post(Routing.generate('sala_get_comentarios', {idSala: $('.marco-sala').attr('id-sala')}), {vez: 2}, function(data) {
+                if (data != ''){                    
+                    $('#chat-mensajes').append(data);   // Añadir el nuevo mensaje al final
+                    setScroll();
+                }
+            });
+        }
+    }, 60000);
+
 });
