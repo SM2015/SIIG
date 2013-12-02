360d4dc249c6754b3e845b3e51cd714be49948d4
rigosv <rigoberto.reyes@gmail.com>
2013-12-01 23:27:06 -0600

    Modificación en el uso del proxy inverso, ahora se verifica si han cambiado datos del indicador para decidir si se usa la caché o no

--- src/MINSAL/IndicadoresBundle/Controller/IndicadorRESTController.php
+++ src/MINSAL/IndicadoresBundle/Controller/IndicadorRESTController.php
@@ -3,14 +3,8 @@
 namespace MINSAL\IndicadoresBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use MINSAL\IndicadoresBundle\Entity\FichaTecnica;
-use MINSAL\IndicadoresBundle\Entity\ClasificacionUso;
-use MINSAL\IndicadoresBundle\Entity\User;
-use MINSAL\IndicadoresBundle\Entity\GrupoIndicadores;
-use MINSAL\IndicadoresBundle\Entity\UsuarioGrupoIndicadores;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
@@ -28,33 +22,47 @@ class IndicadorRESTController extends Controller
      */
     public function getIndicadorAction(FichaTecnica $fichaTec, $dimension)
     {
-        $resp = array();
-        $filtro = $this->getRequest()->get('filtro');
-        $verSql = ($this->getRequest()->get('ver_sql') == 'true') ? true : false;
+        $response = new Response();
+        
+        $response->setETag($fichaTec->getId());
+        $response->setLastModified($fichaTec->getUltimaLectura());
+    
+        $response->setPublic();
+        // verifica que la respuesta no se ha modificado para la petición dada
+        if ($response->isNotModified($this->getRequest()) and $this->get('kernel')->getEnvironment() != 'dev') {
+            // devuelve inmediatamente la respuesta 304 de la caché
+            return $response;
+        } else {
+            $resp = array();            
+            $filtro = $this->getRequest()->get('filtro');
+            $verSql = ($this->getRequest()->get('ver_sql') == 'true') ? true : false;
 
-        if ($filtro == null or $filtro == '')
-            $filtros = null;
-        else {
+            if ($filtro == null or $filtro == '')
+                $filtros = null;
+            else {
 
-            $filtrObj = json_decode($filtro);
-            foreach ($filtrObj as $f) {
-                $filtros_dimensiones[] = $f->codigo;
-                $filtros_valores[] = $f->valor;
+                $filtrObj = json_decode($filtro);
+                foreach ($filtrObj as $f) {
+                    $filtros_dimensiones[] = $f->codigo;
+                    $filtros_valores[] = $f->valor;
+                }
+                $filtros = array_combine($filtros_dimensiones, $filtros_valores);
             }
-            $filtros = array_combine($filtros_dimensiones, $filtros_valores);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $fichaRepository = $em->getRepository('IndicadoresBundle:FichaTecnica');
+
+            $fichaRepository->crearIndicador($fichaTec, $dimension, $filtros);
+            $resp['datos'] = $fichaRepository->calcularIndicador($fichaTec, $dimension, $filtros, $verSql);
+        
+            $response->setContent(json_encode($resp));
+            //Duración de la caché puesta en un año ya que antes se verifica si 
+            //se han leido nuevos datos en el indicador, de ser así se actualiza
+            // sin tomar en cuenta el valor de un año
+            $response->setSharedMaxAge(31556926);
+            
+            return $response;            
         }
-
-        $em = $this->getDoctrine()->getManager();
-
-        $fichaRepository = $em->getRepository('IndicadoresBundle:FichaTecnica');
-
-        $fichaRepository->crearIndicador($fichaTec, $dimension, $filtros);
-        $resp['datos'] = $fichaRepository->calcularIndicador($fichaTec, $dimension, $filtros, $verSql);
-        //return $resp;
-        $response = new Response(json_encode($resp));
-        if ($this->get('kernel')->getEnvironment() != 'dev')
-            $response->setMaxAge($this->container->getParameter('indicador_cache_consulta'));
-
-        return $response;
     }
-}//end class
+}
