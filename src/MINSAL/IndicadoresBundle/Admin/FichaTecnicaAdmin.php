cd3891bed12163643e1b5fe9d8dc17bb0184084f
Eric Ramirez <rajotico@gmail.com>
2013-12-27 14:58:52 -0600

    Update FichaTecnicaAdmin.php

--- src/MINSAL/IndicadoresBundle/Admin/FichaTecnicaAdmin.php
+++ src/MINSAL/IndicadoresBundle/Admin/FichaTecnicaAdmin.php
@@ -636,7 +636,7 @@ FROM ".$tabla." aa
         $q=array(
         'id'=>'7',
         'titulo'=>'Municipios con mas casos | tabla',
-        'sql'=>"Select bb.descripcion as municipio, ".$formula_ageregada. " as casos FROM ".$tabla.
+        'sql'=>"Select bb.descripcion as municipio, ".$formula_agregada. " as casos FROM ".$tabla.
         " aa  ".$where_sql."  group  by bb.descripcion order by casos desc limit 12;" );
         $q['sql']= htmlspecialchars(str_replace('!','${', $q['sql']));
         array_push($queries,$q);
