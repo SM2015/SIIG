798efc754c59754e8170cb2aec62be79a828c74f
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2014-07-21 23:39:51 -0600

    eliminación de caracteres dentro de la fecha de creación del reporte de sala

--- src/MINSAL/IndicadoresBundle/Controller/FichaTecnicaAdminController.php
+++ src/MINSAL/IndicadoresBundle/Controller/FichaTecnicaAdminController.php
@@ -97,7 +97,8 @@ class FichaTecnicaAdminController extends Controller
      */
     public function tableroSalaAction($sala){
         $html = $this->tableroAction($sala);
-        
+        $html = preg_replace("/HTTP.+/","",$html);
+        $html = preg_replace("/Cache.+/","",$html);        
         
         $http = ($_SERVER['HTTPS'] == null or $_SERVER['HTTPS'] == 'off') ? 'http' : 'https';
         
@@ -106,7 +107,7 @@ class FichaTecnicaAdminController extends Controller
                     'src="'.$http.'://'.$_SERVER['HTTP_HOST'].'/bundles',
                     'src="'.$http.'://'.$_SERVER['HTTP_HOST'].'/app_dev.php'), $html);
         
-        return new Response($html);
+        //return new Response($html);
         return new Response(
             $this->get('knp_snappy.pdf')->getOutputFromHtml($html), 200, array(
                 'Content-Type' => 'application/pdf',
