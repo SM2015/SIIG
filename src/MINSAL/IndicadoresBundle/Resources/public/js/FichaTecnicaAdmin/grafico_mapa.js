d3fe960caa3ccb75b8508cd74a4ba79f434c0daa
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2014-07-28 22:04:09 -0600

    Actualización del gráfico de mapa para utilizar el nuevo formato topoJson

--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_mapa.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/grafico_mapa.js
@@ -3,14 +3,14 @@ graficoMapa = function(ubicacion, datos, colorChosen, categoryChoosen) {
     // Esta variable me permite asociar el elmento del gráfico con los códigos utilizados
     // por los departamentos
     this.dibujar = function() {
-        var dimension = $('#' + ubicacion + ' .dimensiones').val();                
+        var dimension = $('#' + ubicacion + ' .dimensiones').val();
         var elemento_id_codigo = new Object();
 
         var width = 370,
-            height = 250,
-            currentDatasetChart = datos,
-            centered,
-            arreglo_datos = new Object();
+                height = 250,
+                currentDatasetChart = datos,
+                centered,
+                arreglo_datos = new Object();
 
         var categorias = currentDatasetChart.map(function(d) {
             return d.id_category;
@@ -18,32 +18,34 @@ graficoMapa = function(ubicacion, datos, colorChosen, categoryChoosen) {
         var medidas = currentDatasetChart.map(function(d) {
             return d.measure;
         });
-        $.each(categorias, function(i, nodo) {            
+        $.each(categorias, function(i, nodo) {
             arreglo_datos[nodo] = medidas[i];
         });
         //Recuperar las coordenadas a utilizar 
-        var coordenadas = $('#' + ubicacion + ' .dimensiones option[value="'+dimension+'"]');
+        var coordenadas = $('#' + ubicacion + ' .dimensiones option[value="' + dimension + '"]');
         coordenadas = $(coordenadas[0]);
         if (coordenadas.attr('data-escala') === 'null'
-            || coordenadas.attr('data-x') === 'null'
-            || coordenadas.attr('data-y') === 'null'){
-                alert(trans.no_mapa);
-                $('#' + ubicacion + ' .tipo_grafico_principal').val('columnas');
-                dibujarGrafico(ubicacion, $('#' + ubicacion + ' .dimensiones').val());
-                return;
+                || coordenadas.attr('data-x') === 'null'
+                || coordenadas.attr('data-y') === 'null') {
+            alert(trans.no_mapa);
+            $('#' + ubicacion + ' .tipo_grafico_principal').val('columnas');
+            dibujarGrafico(ubicacion, $('#' + ubicacion + ' .dimensiones').val());
+            return;
         }
         /* Datos para el mapa de El Salvador
          * .scale(9000)
--                .origin([-88.9, 13.7])
+         -                .origin([-88.9, 13.7])
          */
+        var escala = coordenadas.attr('data-escala');
         var projection = d3.geo.albers()
-                .scale(coordenadas.attr('data-escala'))
-                .origin([coordenadas.attr('data-x'), coordenadas.attr('data-y')])
-                .translate([0, 0]);
+                .scale(escala)
+                .parallels([coordenadas.attr('data-x'), coordenadas.attr('data-y')])
+                .translate([-escala / 9.2, -escala / 2.8]);
+
 
         var path = d3.geo.path()
                 .projection(projection);
-        
+
         $('#' + ubicacion + ' .grafico').html('');
         var svg = d3.select("#" + ubicacion + ' .grafico')
                 .append("svg")
@@ -59,48 +61,53 @@ graficoMapa = function(ubicacion, datos, colorChosen, categoryChoosen) {
                 .attr("height", height)
                 .on("contextmenu", right_click)
                 .on("click", function(d, i) {
-            descenderNivelDimension(ubicacion, d.properties.NAME);
-        });
+                    descenderNivelDimension(ubicacion, d.properties.NAME);
+                });
 
-        var g = svg.append("g")
-                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-                .append("g")
-                .attr("id", "states");
+        var g = svg.append("g");
 
         var filtro = $('#' + ubicacion + ' .filtros_dimensiones').attr('data');
 
         d3.json(Routing.generate('indicador_datos_mapa',
                 {id: $('#' + ubicacion + ' .titulo_indicador').attr('data-id'), dimension: dimension,
                     filtro: filtro, tipo_peticion: 'mapa'}),
-        function(json) {
-            if (json.features === '') {
-                alert(trans.no_mapa);
-                $('#' + ubicacion + ' .tipo_grafico_principal').val('columnas');
-                dibujarGrafico(ubicacion, $('#' + ubicacion + ' .dimensiones').val());
-                return;
-            }
-            var seccion = g.selectAll("path").data(json.features).enter().append("path").attr("d", path);
+                function(datos) {
+                    //+dimension.replace('id_','')
+                    var objetos = 'datos.objects.departamento';
+                    if (topojson.feature(datos, eval(objetos)).features === '') {
+                        alert(trans.no_mapa);
+                        $('#' + ubicacion + ' .tipo_grafico_principal').val('columnas');
+                        dibujarGrafico(ubicacion, $('#' + ubicacion + ' .dimensiones').val());
+                        return;
+                    }
 
-            seccion.append("title").
-                    text(function(d) {
-                if (arreglo_datos[d.properties.ID] === undefined)
-                    return d.properties.NAME;
-                else
-                    return d.properties.NAME + ': ' + arreglo_datos[d.properties.ID];
-            });
+                    var seccion = g.append("g")
+                            .attr("id", "states")
+                            .selectAll("path")
+                            .data(topojson.feature(datos, eval(objetos)).features)
+                            .enter().append("path")
+                            .attr("d", path)
+                            .attr('fill', function(d, i) {
+                                if (arreglo_datos[d.properties.ID] === undefined)
+                                    return '#E5DBDB';
+                                else
+                                    return colores_alertas(ubicacion, arreglo_datos[d.properties.ID], i);
+                            });
 
-            seccion.attr('fill', function(d, i) {
-                if (arreglo_datos[d.properties.ID] === undefined)
-                    return '#E5DBDB';
-                else
-                    return colores_alertas(ubicacion, arreglo_datos[d.properties.ID], i);
-            });
-            seccion.on("contextmenu", right_click)
-                    .on("click", function(d, i) {
-                if (arreglo_datos[d.properties.ID] !== undefined)
-                    descenderNivelDimension(ubicacion, d.properties.NAME);
-            });
-        });
+                    seccion.append("title").
+                            text(function(d) {
+                                if (arreglo_datos[d.properties.ID] === undefined)
+                                    return d.properties.NAME;
+                                else
+                                    return d.properties.NAME + ': ' + arreglo_datos[d.properties.ID];
+                            });
+
+                    seccion.on("contextmenu", right_click)
+                            .on("click", function(d, i) {
+                                if (arreglo_datos[d.properties.ID] !== undefined)
+                                    descenderNivelDimension(ubicacion, d.properties.NAME);
+                            });
+                });
 
         function right_click(d) {
             var x = 0,
@@ -109,8 +116,8 @@ graficoMapa = function(ubicacion, datos, colorChosen, categoryChoosen) {
 
             if (d && centered !== d) {
                 var centroid = path.centroid(d);
-                x = -centroid[0];
-                y = -centroid[1];
+                x = -centroid[0] + 40;
+                y = -centroid[1] + 30;
                 k = 4;
                 centered = d;
             } else {
@@ -119,8 +126,8 @@ graficoMapa = function(ubicacion, datos, colorChosen, categoryChoosen) {
 
             g.selectAll("path")
                     .classed("active", centered && function(d) {
-                return d === centered;
-            });
+                        return d === centered;
+                    });
 
             g.transition()
                     .duration(1000)
