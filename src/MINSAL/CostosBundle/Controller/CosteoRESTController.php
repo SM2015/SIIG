57cb36c54965bba4341c88ea072ca8600dd99f40
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2014-11-27 22:18:30 -0600

    Agregada opción del menú para resumen de costos de recurso humano

--- src/MINSAL/CostosBundle/Controller/CosteoRESTController.php
+++ src/MINSAL/CostosBundle/Controller/CosteoRESTController.php
@@ -9,6 +9,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 
 class CosteoRESTController extends Controller {
+
     /**
      * Obtener los datos del costeo
      * @Get("/rest-service/costos/data/{codigo}", options={"expose"=true})
@@ -16,7 +17,7 @@ class CosteoRESTController extends Controller {
      */
     public function getDatosCosteoAction($codigo) {
         $response = new Response();
-        
+
         // crea una respuesta con una cabecera ETag y Last-Modified
         // para determinar si se debe calcular el indicador u obtener de la caché
         // para el modo de desarrollo (dev) nunca tomar de caché
@@ -26,23 +27,23 @@ class CosteoRESTController extends Controller {
         $response->setPublic();
         // verifica que la respuesta no se ha modificado para la petición dada
         //if ($response->isNotModified($this->getRequest())) {
-            // devuelve inmediatamente la respuesta 304 de la caché
-          //  return $response;
+        // devuelve inmediatamente la respuesta 304 de la caché
+        //  return $response;
         //} else {
-            $resp = array();            
+        $resp = array();
 
-            $em = $this->getDoctrine()->getManager();
-            
-            $data = $em->getRepository('CostosBundle:Formulario')->getDatosCosteo($codigo);
-            
-            if (count($data) == 0){
-                $response->setContent('{"estado" : "error", "msj": "' . $this->get('translator')->trans('_no_datos_') . '"}');
-            }
-            else {
-                $response->setContent(json_encode($data));
-            }
+        $em = $this->getDoctrine()->getManager();
 
-            return $response;
+        $data = $em->getRepository('CostosBundle:Formulario')->getDatosCosteo($codigo);
+
+        if (count($data) == 0) {
+            $response->setContent('{"estado" : "error", "msj": "' . $this->get('translator')->trans('_no_datos_') . '"}');
+        } else {
+            $response->setContent(json_encode($data));
+        }
+
+        return $response;
         //}
     }
+
 }
