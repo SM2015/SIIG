238657955169c2209a9fe954319e1dc24071d414
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2015-04-16 21:14:23 -0600

    Corrección de error no guardaba filtro aplicado

--- src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/tablero.js
+++ src/MINSAL/IndicadoresBundle/Resources/public/js/FichaTecnicaAdmin/tablero.js
@@ -159,7 +159,7 @@ $(document).ready(function() {
                     elementos.push($(this).val());
                 });
                 datos.id_indicador = $(this).find('.titulo_indicador').attr('data-id');
-                datos.filtros = $(this).children('.filtros_dimensiones').attr('data');
+                datos.filtros = $(this).find('.filtros_dimensiones').attr('data');
                 datos.filtro_desde = $('#' + $(this).attr('id') + ' .filtro_desde').val();
                 datos.filtro_hasta = $('#' + $(this).attr('id') + ' .filtro_hasta').val();
                 datos.filtro_elementos = elementos.toString();
